d3215468bf1cce1df260a8c24e75799b9c3ebfdd
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2024-07-12 14:41:13 +0500

    Init Commit

---
+++ src/main/java/dasturlash/uz/SpringSecurityDefaultImplementationApplication.java
@@ -0,0 +1,13 @@
+package dasturlash.uz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDefaultImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDefaultImplementationApplication.class, args);
+	}
+
+}
